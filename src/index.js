c77cb97b0cfc2aa2fb45bd74439146c9e5165be1
kiwenlau <kiwenlau@126.com>
2018-10-29 20:30:13 +0800

    配置ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -5,13 +5,44 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 var fundebug = require("fundebug-javascript");
-fundebug.apikey =
-    "f24f3b42fd3124c501961746c4768e0945edaefda04b58d84f30a1137b33be13";
+// 配置apikey
+fundebug.apikey = "API-KEY";
 
 // TEST01: 测试fundebug.notify接口，阔以收到报警邮件
 fundebug.notify("TEST 01", "Hello, Fundebug!");
 
-ReactDOM.render(<App />, document.getElementById("root"));
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true });
+        // 将component中的报错发送到Fundebug
+        fundebug.notifyError(error, {
+            metaData: {
+                info: info
+            }
+        });
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return null;
+            // Note: 也可以在出错的component处展示出错信息，返回自定义的结果。
+        }
+        return this.props.children;
+    }
+}
+
+ReactDOM.render(
+    <ErrorBoundary>
+        {" "}
+        <App />{" "}
+    </ErrorBoundary>,
+    document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
